e1a57c49a9a763072ba80312ca903a6773406b13
Brian Cilenti <briancilenti@gmail.com>
2019-12-15 17:01:00 -0500

    done

--- index.js
+++ index.js
@@ -1,47 +1,36 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type Fiz {
-        alpha: String
-    }
-
-    type Foo {
-        bar: String,
-        fiz: [Fiz]
+    type Product {
+        name: String
+        description: String
     }
 
     type Query {
-        hello: String,
-        helloTwo: String,
-        myFoo: Foo
+        product(id: ID!): Product
     }
 `;
 
 /* Apollo server allows us to customize the mocks for a given query */
 
 const mocks = {
-    String: () => 'my custom string value' /* since the scalar type String starts with uppercase "S",
-    the entry in the mocks object must also */
+    String: () => 'product something or other'
 }
 
 /* We can mock resolvers if we'd like. */
 
-const resolvers = {
-    Query: {
-        helloTwo: () => {
-            return 'helloTwo\'s string response'
-        }
-    }
-}
+// const resolvers = {
+//     Query: {
+//         helloTwo: () => {
+//             return 'helloTwo\'s string response'
+//         }
+//     }
+// }
 
 const server = new ApolloServer({
     typeDefs,
-    /* Activate mock mode. Allows us to not need to write resolvers. Apollo will
-    automatically return objects based on the return type provided in typeDefs */
-    // mocks: true // use this when you don't want to provide any custom mocks.
-    mocks, // mocks resolvers per your mocks option config.
-    resolvers,
-    mockEntireSchema: false // must use this if providing custom resolvers.
+    mocks,
+    mockEntireSchema: false 
 
 });
 
